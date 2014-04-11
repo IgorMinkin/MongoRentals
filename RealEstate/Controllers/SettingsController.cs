fbec0e118fdbc5d7b8265b910b6bef1a6a91c242
Igor Minkin <igor.minkin@gmail.com>
2014-04-10 20:15:11 -0400

    step 1

---
+++ RealEstate/Controllers/SettingsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RealEstate.Models;
+
+namespace RealEstate.Controllers
+{
+    public class SettingsController : Controller
+    {
+        //
+        // GET: /Settings/
+        [ChildActionOnly]
+        public ActionResult Get()
+        {
+            return PartialView("GetSettings", new AppSettings());
+        }
+	}
+}
---
+++ RealEstate/Models/AppSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Dynamic;
+using Newtonsoft.Json;
+
+namespace RealEstate.Models
+{
+    public class AppSettings
+    {
+        private static readonly IDictionary<string,object> Settings = new Dictionary<string, object>();
+
+        static AppSettings ()
+        {
+            var appSettingsKeys = ConfigurationManager.AppSettings.AllKeys;
+
+            foreach (var appSettingsKey in appSettingsKeys)
+            {
+                Settings[appSettingsKey] = ConfigurationManager.AppSettings[appSettingsKey];
+            }
+        }
+
+        public string ToJSON()
+        {
+            var settings = JsonConvert.SerializeObject(Settings);
+            Debug.WriteLine(settings);
+            return settings;
+        }
+    }
+}
