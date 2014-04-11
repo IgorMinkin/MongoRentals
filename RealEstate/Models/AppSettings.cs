5de9c56a5ac6ab947d00230d60d8091de9868fcf
Igor Minkin <igor.minkin@gmail.com>
2014-04-11 06:16:08 -0400

    minor refactoring

--- RealEstate/Models/AppSettings.cs
+++ RealEstate/Models/AppSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
 using System.Dynamic;
@@ -8,23 +9,24 @@ namespace RealEstate.Models
 {
     public class AppSettings
     {
-        private static readonly IDictionary<string,object> Settings = new Dictionary<string, object>();
+        private static readonly Lazy<IDictionary<string,object>> Settings = new Lazy<IDictionary<string, object>>(ReadSettings);
 
-        static AppSettings ()
+        private static IDictionary<string, object> ReadSettings()
         {
+            var settings = new Dictionary<string, object>();
             var appSettingsKeys = ConfigurationManager.AppSettings.AllKeys;
 
             foreach (var appSettingsKey in appSettingsKeys)
             {
-                Settings[appSettingsKey] = ConfigurationManager.AppSettings[appSettingsKey];
+                settings[appSettingsKey] = ConfigurationManager.AppSettings[appSettingsKey];
             }
-        }
 
-        public string ToJSON()
-        {
-            var settings = JsonConvert.SerializeObject(Settings);
-            Debug.WriteLine(settings);
             return settings;
+        } 
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(Settings.Value);
         }
     }
 }
